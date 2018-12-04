27583d052c92fa85f3742938ba98392252d1e2df
Ralph <ralphe1271@gmail.com>
2018-12-04 11:54:56 -0500

    fffff

--- server.js
+++ server.js
@@ -33,11 +33,11 @@ if(process.env.NODE_ENV === 'production') {
     // set static folder
     app.use(express.static('client/build'));
 
-    app.get('*', (res,req) => {
+    app.get('/', (res,req) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
